207296d8f752f0eafee0d1ddf5292c330813f8b8
Akbar Maulana Rizki <akbarmaulanarizki@gmail.com>
2026-06-03 08:31:41 +0700

    debuging auth.js

--- js/auth.js
+++ js/auth.js
@@ -10,15 +10,15 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase
 // ============ KONFIGURASI FIREBASE ============
 // Ambil dari Firebase Console -> Project Settings -> General -> Your apps
 const firebaseConfig = {
-    apiKey: "AIzaSyDEFAULT_PLACEHOLDER_REPLACE_ME",     // GANTI DENGAN API KEY KAMU
-    authDomain: "ujikom-2026.firebaseapp.com",          // GANTI
-    databaseURL: "https://ujikom-2026-default-rtdb.asia-southeast1.firebasedatabase.app", // GANTI
-    projectId: "ujikom-2026",                           // GANTI
-    storageBucket: "ujikom-2026.firebasestorage.app",   // GANTI
-    messagingSenderId: "123456789012",                  // GANTI
-    appId: "1:123456789012:web:abcdef123456"            // GANTI
+  apiKey: "AIzaSyDmtJgn8kXFAxw5FAmQrnaabBoHj5-dcbA",
+  authDomain: "ujikom-2026.firebaseapp.com",
+  databaseURL: "https://ujikom-2026-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ujikom-2026",
+  storageBucket: "ujikom-2026.firebasestorage.app",
+  messagingSenderId: "201690433163",
+  appId: "1:201690433163:web:90a7f79dad26a83e25f68e",
+  measurementId: "G-6F99EGS7EF"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
